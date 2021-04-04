c3a12acda68cf5dd78ef115a401ffe4a1132d39f
Madhuwantha <madhuwanthapriyashan12@gmail.com>
2021-04-04 21:35:07 +0530

    authentication with keyclock is configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestRestrctedComponent } from './test-restrcted/test-restrcted.component';
+import {KeycloakAngularModule, KeycloakService} from 'keycloak-angular';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {KeycloakHttpInterceptorServiceService} from './keycloak-http-interceptor-service.service';
+
+// tslint:disable-next-line:typedef
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080/auth',
+        realm: 'heroes',
+        clientId: 'spa-heroes-backend',
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html',
+      },
+      enableBearerInterceptor: true,
+      loadUserProfileAtStartUp: true
+    });
+}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestRestrctedComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, BrowserModule, KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: KeycloakHttpInterceptorServiceService, multi: true },
+    { provide: APP_INITIALIZER, useFactory: initializeKeycloak, multi: true, deps: [KeycloakService]},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/test-restrcted/test-restrcted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-restrcted',
+  templateUrl: './test-restrcted.component.html',
+  styleUrls: ['./test-restrcted.component.css']
+})
+export class TestRestrctedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
